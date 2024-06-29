c98e4be13d05d89f05d48dcbead10092ae329c90
Luiz Gustavo <gutfoda@gmail.com>
2024-06-29 13:50:20 -0300

    Criação do relatorio

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Contatos.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap impressora {
+            get {
+                object obj = ResourceManager.GetObject("impressora", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pexels_francesco_ungaro_281260 {
             get {
                 object obj = ResourceManager.GetObject("pexels-francesco-ungaro-281260", resourceCulture);
